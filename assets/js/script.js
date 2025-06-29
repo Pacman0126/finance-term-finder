3da7b6fe5df7b92221c45ba1681f381aff072c6f
Pacman0126 <oliver.p.hartmann@gmail.com>
2025-06-29 23:39:33 +0200

    add calculateNumberOfPayments() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,46 @@ document.addEventListener("DOMContentLoaded", function () {
 
         })
     }
+})
+function calculateTerms(selectComputeAction) {
 
-    function calculateTerms() {
-
-
+    switch (selectComputeAction) {
+        case "calcOnUserSelection":
+            calculateNumberOfPayments();
+            break;
+        case "adjustMaxMonPmt":
+            // code block
+            break;
+        default:
+        // code block
     }
 
+}
+/**
+ * PV = present value i.e. principal
+ * A = monthly payment
+ * annualInterestRate = lender's annaul interest rate
+ * paymentIntervals = number of payments
+ */
+function calculateNumberOfPayments() {
 
-})
+    let PV = parseFloat(document.getElementById("finance-amount").value);
+    let A = parseFloat(document.getElementById("max-mon-payment").value);
+    let annualInterestRate = parseFloat(document.getElementById("annual-interest").value);
+
+    let targetPtoARatio = Math.log(PV / A);
+
+    let paymentIntervals = 2;
+    let PtoARatio = Math.log(paymentIntervals) - Math.log(1 + ((annualInterestRate * .01) / paymentIntervals));
+
+    while (PtoARatio <= targetPtoARatio) {
+        paymentIntervals++;
+        PtoARatio = Math.log(paymentIntervals) - Math.log(1 + ((annualInterestRate * .01) / paymentIntervals));
+
+        if (PtoARatio >= targetPtoARatio) {
+            return paymentIntervals
+
+        }
+    }
+
+}
